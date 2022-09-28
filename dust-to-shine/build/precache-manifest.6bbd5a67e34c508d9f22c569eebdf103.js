9d223d245c93fd6010376276417b8d4638416d60
Brandon Archuleta <brandonkarchul@outlook.com>
2022-09-28 12:04:19 -0600

    chore:added build file

---
+++ dust-to-shine/build/precache-manifest.6bbd5a67e34c508d9f22c569eebdf103.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "821df7f1bd514e8e0109",
+    "url": "/static/css/main.6e1c14c4.chunk.css"
+  },
+  {
+    "revision": "821df7f1bd514e8e0109",
+    "url": "/static/js/main.684c0646.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a36229e96f86d640a57c",
+    "url": "/static/css/2.24ca775a.chunk.css"
+  },
+  {
+    "revision": "a36229e96f86d640a57c",
+    "url": "/static/js/2.c7dcd903.chunk.js"
+  },
+  {
+    "revision": "a7427534cb06cfb9feb627d27262b029",
+    "url": "/index.html"
+  }
+];
